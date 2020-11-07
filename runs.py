c4bc9227983aff64976f1be67182ff6ab57004a4
Tom Ritchford <tom@swirly.com>
2020-11-07 15:05:51 +0100

    More sketching

--- runs.py
+++ runs.py
@@ -3,14 +3,37 @@
 ------------------------------------------------------------------
 """
 
-import subprocess
+import functools
+import shlex
+import subprocess as sp
 import xmod
 
-__all__ = 'runs'
-__version__ = '0.1.0'
+__all__ = 'call', 'check_call', 'check_output', 'run', 'runs'
+__version__ = '0.2.0'
 
 
-@xmod
-def runs():
-    """Run one or more commands"""
-    return subprocess and None
+def _wrap(f):
+    @functools.wraps(f)
+    def wrapped(cmd, *args, **kwargs):
+        assert isinstance(cmd, str)
+
+        lines = (c.strip() for c in cmd.splitlines())
+        lines = (c for c in lines if c)
+        if not kwargs.get('shell'):
+            lines = (shlex.strip(c) for c in lines)
+
+        for c in lines:
+            yield f(c, *args, **kwargs)
+
+    return wrapped
+
+
+call = _wrap(sp.call)
+
+check_call = _wrap(sp.check_call)
+
+check_output = _wrap(sp.check_output)
+
+run = _wrap(sp.run)
+
+runs = xmod(run)
--- test_runs.py
+++ test_runs.py
@@ -1,13 +1,14 @@
-from unittest import mock
+# from unittest import mock
+# import tdir
+
 import runs
-import tdir
 import unittest
 
 FILENAME = 'a_file.txt'
 
 
-@mock.patch('runs.subprocess.call', autospec=True)
+# @mock.patch('runs.subprocess.call', autospec=True)
 class TestRuns(unittest.TestCase):
-    @tdir(FILENAME)
-    def test_existing(self, call):
-        assert runs() is None
+    #   @tdir(FILENAME)
+    def test_existing(self):
+        assert runs
