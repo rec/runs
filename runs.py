5dee4ad930acf0dd8e4725ed10cd36df1a4e25ad
Tom Ritchford <tom@swirly.com>
2020-11-08 17:06:55 +0100

    Final sketches

--- runs.py
+++ runs.py
@@ -12,20 +12,46 @@ __version__ = '0.2.0'
 __all__ = 'call', 'check_call', 'check_output', 'run'
 
 
-def _wrap(name):
-    @functools.wraps(getattr(subprocess, name))
-    def wrapped(cmd, *args, on_exception=None, **kwargs):
-        for c in _split(cmd, kwargs.get('shell')):
-            function = getattr(subprocess, name)
+def _run(name, cmd, *args, on_exception=None, echo=False, **kwargs):
+    echo = echo or (lambda *a: None)
+    if echo is True:
+        echo = '$'
+    if isinstance(echo, str):
+        echo = functools.partial(print, echo)
+
+    assert callable(echo)
+
+    function = getattr(subprocess, name)
+    shell = kwargs.get('shell')
+
+    for line in cmd.splitlines():
+        echo(line)
+        cmd = shlex.split(line, comments=True)
+
+        if cmd:
+            if shell:
+                cmd = ' '.join(shlex.quote(c) for c in cmd)
+
             try:
-                result = function(c, *args, **kwargs)
+                result = function(cmd, *args, **kwargs)
+
             except Exception:
                 if not on_exception:
                     raise
-                elif callable(on_exception):
-                    on_exception(c)
+                if on_exception is not True:
+                    on_exception(cmd)
+
             else:
                 yield result
+
+
+def _wrap(name):
+    @functools.wraps(getattr(subprocess, name))
+    def wrapped(cmd, *args, iterate=False, **kwargs):
+        it = _run(name, cmd, *args, **kwargs)
+        if iterate:
+            return it
+        return list(it)
 
     return wrapped
 
@@ -35,13 +61,4 @@ check_call = _wrap('check_call')
 check_output = _wrap('check_output')
 run = _wrap('run')
 
-runs = xmod(run, __name__)
-
-
-def _split(lines, shell):
-    lines = (i.strip() for i in lines.splitlines())
-    lines = (i for i in lines if i)
-    if shell:
-        return lines
-
-    return (shlex.split(i) for i in lines)
+xmod(run, __name__)
